726e44bb2c9c7101153906cf439ad1544fc9704c
Paul Serby <paul.serby@clock.co.uk>
2011-06-25 13:38:34 +0200

    Adding doc comment to decamelcase

--- index.js
+++ index.js
@@ -6,8 +6,13 @@
  * Thursday 25 June 2011
  *
  */
-module.exports = {
-	decamelcase: function(value) {
-		return value.substring(0, 1).toUpperCase() + value.substring(1).replace(/([^\s0-9])([A-Z0-9])/g, function($0, $1, $2) { return $1 + " " + $2; });
-	}
+
+/**
+ *
+ * Takes a string in the form 'addressLine1' and converts to 'Address Line 1'
+ *
+ * @param {String} value The string to be converted
+ */
+exports.decamelcase = function(value) {
+	return value.substring(0, 1).toUpperCase() + value.substring(1).replace(/([^\s0-9])([A-Z0-9])/g, function($0, $1, $2) { return $1 + " " + $2; });
 };
