493f207032139fc3b180347b2e8adc8c46fe8415
Paul Serby <paul.serby@clock.co.uk>
2011-12-18 00:27:07 +0000

    tests now in mocha

--- test/decamelcase.test.js
+++ test/decamelcase.test.js
@@ -2,29 +2,41 @@ var
 	stringUtils = require('../../piton-string-utils'),
 	assert = require('assert');
 
-module.exports = {
-	'empty string returns empty string': function() {
-		assert.strictEqual('', stringUtils.decamelcase(''));
-	},
-	'title case word is unaffected': function() {
-		assert.strictEqual('Hello', stringUtils.decamelcase('Hello'));
-	},
-	'title case words are unaffected': function() {
-		assert.strictEqual('Hello World', stringUtils.decamelcase('Hello World'));
-	},
-	'camelcase phrase is correctly converted': function() {
-		assert.strictEqual('Hello World', stringUtils.decamelcase('helloWorld'));
-	},
-	'camelcase with upper case first charater get converted': function() {
-		assert.strictEqual('Hello World', stringUtils.decamelcase('HelloWorld'));
-	},
-	'a single lower case words has first letter converted to uppercase': function() {
-		assert.strictEqual('Hello World', stringUtils.decamelcase('Hello World'));
-	},
-	'numbers get spaces insert before them': function() {
-		assert.strictEqual('Address Line 1', stringUtils.decamelcase('addressLine1'));
-	},
-	'numbers with >1 digits convert correctly': function() {
-		assert.strictEqual('Gate 1452', stringUtils.decamelcase('gate1452'));
-	}
-};
+describe('piton-string-utils', function() {
+
+	describe('#decamelcase()', function() {
+
+		it('empty string returns empty string', function() {
+			''.decamelcase().should.equal('');
+		});
+
+		it('title case word is unaffected', function() {
+			'Hello'.decamelcase().should.equal('Hello');
+		});
+
+		it ('title case words are unaffected', function() {
+			'Hello World'.decamelcase().should.equal('Hello World');
+		});
+
+		it ('camelcase phrase is correctly converted', function() {
+			'helloWorld'.decamelcase().should.equal('Hello World');
+		});
+
+		it('camelcase with upper case first charater get converted', function() {
+			'HelloWorld'.decamelcase().should.equal('Hello World');
+		});
+
+		it('a single lower case words has first letter converted to uppercase', function() {
+			'Hello World'.decamelcase().should.equal('Hello World');
+		});
+
+		it('numbers get spaces insert before them', function() {
+			'addressLine1'.decamelcase().should.equal('Address Line 1');
+		});
+
+		it('numbers with >1 digits convert correctly', function() {
+			'gate1452'.decamelcase().should.equal('Gate 1452');
+		});
+
+	});
+});
